7200aea99cfaf1729a6f9ca2673f66d96be65e28
andrey-simoes <andrey@mc360.com.br>
2022-03-05 19:40:00 -0300

    first commig

---
+++ script.js
@@ -0,0 +1,29 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+
+let closeMenu = document.getElementById('menu');
+
+let activeMenu = document.getElementById('nav');
+
+function fecharMenu(){
+    activeMenu.classList.remove('active');
+}
+
+closeMenu.addEventListener('click', fecharMenu);
+
