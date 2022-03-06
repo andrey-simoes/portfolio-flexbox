8cb92ce17d28021f3597f3d6d78e9f3ace268757
andrey-simoes <andrey@mc360.com.br>
2022-03-05 22:02:43 -0300

    galeria com modal

--- script.js
+++ script.js
@@ -27,3 +27,25 @@ function fecharMenu(){
 
 closeMenu.addEventListener('click', fecharMenu);
 
+
+let imagens= document.querySelectorAll('.small_img');
+let modal = document.querySelector('.modal-container');
+let modalImg = document.querySelector('#modal_img');
+let btClose = document.querySelector('.fechar');
+let srcVal="";
+
+for(let i =0; i<imagens.length;i++){
+    imagens[i].addEventListener('click',function(){
+        
+        srcVal = imagens[i].getAttribute('src');
+        modalImg.setAttribute('src', srcVal);
+        modal.classList.toggle('mostrar');
+    });
+    
+}
+
+btClose.addEventListener('click', function(){
+  modal.classList.toggle('mostrar');
+});
+
+
